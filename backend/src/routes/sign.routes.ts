ff7923c486cc5f68235961a463ce99df02b25dbf
amine <jmalmohamedamine1@gmail.com>
2025-02-04 11:42:56 +0100

    add logout

--- backend/src/routes/sign.routes.ts
+++ backend/src/routes/sign.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import  signup  from "../controllers/sign.controller/signup.controller";
 import  signin  from "../controllers/sign.controller/signin.controller";
+import logout from "../controllers/sign.controller/logout.controller";
 const router = Router();
 
 router.post("/signup", signup );
 router.get("/signin", signin );
+router.post("/logout", logout );
 
 export default router;
